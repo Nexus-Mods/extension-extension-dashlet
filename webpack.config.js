f67282aaa86dbeef71492e9f761fec439c37626f
TanninOne <seppleviathan@gmx.de>
2023-03-09 15:24:39 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
-let webpack = require('vortex-api/bin/webpack').default;
+const webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('extensions-dashlet', __dirname, 4);
+module.exports = webpack('extensions-dashlet', __dirname, 5);
